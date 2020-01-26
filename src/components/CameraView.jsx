16419cef6d8aff1a46da47ba7d1311c205adc78e
Joshua Gammage <joshuapgammage@gmail.com>
2020-01-25 20:43:33 -0600

    Add camera to see my ugly face

--- src/components/CameraView.jsx
+++ src/components/CameraView.jsx
@@ -4,10 +4,18 @@ import './CameraView.css';
 export default class CameraView extends React.Component {
     render() {
         return <div className="camera-view">
-            <img
-                alt="stupid"
-                src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-            ></img>
+            <video
+                autoPlay
+                id="camera-output"
+                class="camera-output"
+            ></video>
         </div>
+    }
+    componentDidMount() {
+        const video = document.getElementById("camera-output");
+
+        navigator.mediaDevices.getUserMedia({
+            video: true
+        }).then((stream) => { video.srcObject = stream; })
     }
 }
